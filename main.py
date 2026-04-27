72b244211ca90baf861bbbc621e8a3a63119a818
student-cybersec <maltego.umcs@wp.pl>
2026-04-27 11:46:45 +0200

    Update main.py via CodePad [2026-04-27T09:46:44Z]

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 # Welcome to CodePad
 # Edit, run, and push your Python code
 
-
-
 def greet(name):
     return f"Hello, {name}!"
 
 result = greet("World")
 print(result)
-
-import os
-
-for key, value in os.environ.items():
-    print(f"{key}={value}")
